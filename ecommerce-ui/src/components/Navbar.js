0c0af1e0083f12d7c48a9cfbaa9115631fa9a30b
Katrinagd <katrinaguyduy@gmail.com>
2019-04-28 11:40:10 -0700

    Added styled-components

--- ecommerce-ui/src/components/Navbar.js
+++ ecommerce-ui/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
-
-
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary
+      <NavWrapper className="navbar navbar-expand-sm
       navbar-dark px-sm-5">
       {/*
       https://www.iconfinder.com/icons/1417629/accomodation_adventure_airbnb_explore_hotel_travel_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk */}
@@ -22,13 +22,23 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite)!important;
+  font-size:1.3rem;
+  text-transform:capitalize;
+}
+`
