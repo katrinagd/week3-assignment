4557c0f57db3ac485b1b8b26f05d79c3acc381b7
Katrinagd <katrinaguyduy@gmail.com>
2019-05-04 20:04:25 -0700

    Display and style products

---
+++ ecommerce-ui/src/context.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { storeProducts } from './data';
+const ProductContext = React.createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+    state = {
+        products: []
+    };
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts = () =>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem];
+
+        });
+        this.setState(()=>{
+            return {products:tempProducts}
+        });
+    };
+    handleDetail = () =>{
+        console.log('hello from detail');
+    };
+    addToCart = () =>{
+        console.log('hello from cart');
+    };
+    render() {
+        return (
+        <ProductContext.Provider value={{
+            ...this.state,
+            handleDetail:this.handleDetail,
+            addToCart:this.handleDetail,
+        }}>
+            {this.props.children}
+        </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
