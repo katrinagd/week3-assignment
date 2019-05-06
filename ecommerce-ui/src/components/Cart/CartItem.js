2c13a01ce1e0606dcefa347a04f4cc5fab94b222
Katrinagd <katrinaguyduy@gmail.com>
2019-05-05 22:14:53 -0700

    cart component

--- ecommerce-ui/src/components/Cart/CartItem.js
+++ ecommerce-ui/src/components/Cart/CartItem.js
@@ -4,7 +4,7 @@ export default function CartItem({ item,value }) {
     const { id,title,image,payment:{cost},total,count } = item;
     const { increment,decrement,removeItem } = value;
     return (
-        <div className="row my-1 text-capitalize text-center">
+        <div className="row my-2 text-capitalize text-center">
             <div className="col-10 mx-auto col-lg-2">
                 <img 
                     src={image}
